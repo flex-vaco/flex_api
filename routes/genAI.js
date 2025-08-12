0431ed6d06b10d47cb4e0a6894a6b59e94536794
rajvaco <rvanamala@vaco.com>
2025-08-12 16:00:32 +0530

    added user session id for ai chat

--- routes/genAI.js
+++ routes/genAI.js
@@ -8,12 +8,5 @@ const verifyToken = require('../lib/verifyJWToken.js');
 router.use(bodyParser.urlencoded({ extended: true }));
 
 router.get("/talk_to_ai", verifyToken, GenAI.phi_data_agents);
-// router.get("/getTechnologies", verifyToken, Application.getTechnologies);
-// router.post("/sendEmail", verifyToken, Application.sendEmail);
-// router.get("/getChatResp", verifyToken, Application.getChatResp);
-// router.get("/runUserQuery", verifyToken, Application.runUserQuery);
-// router.post("/saveUserQuery", verifyToken, Application.saveUserQuery);
-// router.get("/getUserQueries", verifyToken, Application.getUserQueries);
-
 
 module.exports = router;
